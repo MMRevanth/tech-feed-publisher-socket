19c9c68a789ea37d441408f4790332263bb6c38f
MMRevanth <mmrevanth00@gmail.com>
2020-07-08 23:46:20 +0530

    recent commit

--- guest_book.py
+++ guest_book.py
@@ -1,31 +1,16 @@
 from flask import Flask, render_template, request,url_for ,redirect
 from flask_mail import Mail,Message
-import sqlite3,getpass
-email,password='',''
-email_list=[]
+from datetime import datetime,timedelta
+import sqlite3,os,time,random
+
 app = Flask(__name__)
-mail=Mail(app)
 
-def config():
-	app.config['MAIL_SERVER']='smtp.gmail.com'
-	app.config['MAIL_PORT'] = 465
-	app.config['MAIL_USERNAME'] = email
-	app.config['MAIL_PASSWORD'] = password
-	app.config['MAIL_USE_TLS'] = False
-	app.config['MAIL_USE_SSL'] = True
-	mail=Mail(app)
+Data = sqlite3.connect(os.path.abspath(os.path.dirname(__file__))+'/Database/techfeeds.db',check_same_thread=False)
 
-def create_connection(db_file):
-	try:
-		data=sqlite3.connect(db_file,check_same_thread=False)
-		return data
-	except:
-		print("error while by conneting  to database")
 
-def retrive(data):
-	re=data.cursor()
-	re=re.execute("SELECT feed_links FROM techfeeds")
-	return re.fetchall()
+def retrive(data,table_name,something_else=''):
+	data=data.execute("select * from "+table_name+something_else)
+	return data.fetchall()
 
 @app.route('/')
 def home():
@@ -37,33 +22,53 @@ def subscribe():
 
 @app.route('/result',methods=['GET','POST'])
 def result():
-	if request.method=='POST':
-		attemped_username =request.form['username']
-		attemped_email_id =request.form['email_id']
-		user_data=create_connection('../tech-feed-publisher-socket/Database/userdata.db')
-		try:
-			user_data.execute('CREATE TABLE user(name varchar(20),email varchar(20))')
-		except:
-			pass
-		user_data.execute('INSERT INTO user VALUES(?,?)',(attemped_username,attemped_email_id))
-		ud=user_data.cursor()
-		for row in ud.execute('SELECT email FROM user'):
-			email_list.append(row[0])
-			
-		return render_template('result.html')	
-	else:
-		return render_template('subscribe.html')
-		
+	try:
+		Data.execute('CREATE TABLE user(si integer primary key,name varchar(20),email varchar(50),start_Datetime datetime,interval int,next_feed datetime,subscribe int,tag varchar(10)')
+	except:
+		pass
+	presTime=datetime.today()
+	Data.execute('INSERT INTO user(name,email,start_Datetime,interval,next_feed,subscribe,tag) VALUES(?,?,?,?,?,?,?)',(request.form['username'],request.form['email_id'],str(presTime),request.form['time'],str(presTime+timedelta(minutes=int(request.form['time']))),1,request.form['tag']))
+	Data.commit()
+	return render_template('result.html')	
+
 @app.route('/send')
 def send():
-	main=create_connection("../tech-feed-publisher-socket/Database/techfeeds.db")
-	msg=Message('hello',sender=email,recipients=email_list)
-	msg.body="Thank you for subscribing to our page \n Here are the links for articles: \n"+ str(retrive(main))
-	mail.send(msg)
-	email_list.pop()
-	return "sent"
+	return "thank you"
 
-email=input("Enter your emailid:")
-password=getpass.getpass()
-config()
-app.run()
+@app.route('/admin')
+def admin():
+	return render_template('adminSignin.html')
+
+@app.route('/j_acegi_security_check',methods=['GET','POST'])
+def Check():
+	if request.form['j_username']=='admin' and request.form['j_password']=='admin':
+		data=retrive(Data,'feed')
+		return render_template('db_table.html',data=data)
+	else:
+		return render_template('adminSignin.html',info="please check username or password")
+
+@app.route('/save',methods=['GET','POST'])	
+def add():
+	opt=request.form['opt']
+	if opt=='add':
+		test=Data.execute("""select * from feed where feed_links = ?""",[request.form['feedLinks']])
+		if len(test.fetchall()) == 0:
+			Data.execute('insert into feed(feed_links,titles,tags,summary) values(?,?,?,?)',(request.form['feedLinks'],request.form['titles'],request.form['tags'],request.form['summary']))		
+			info = "new feed inserted"
+		test=Data.execute("""select * from feed where feed_links = ?""",[request.form['feedLinks1']])
+		if len(test.fetchall()) == 0:
+			Data.execute('insert into feed(feed_links,titles,tags,summary) values(?,?,?,?)',(request.form['feedLinks1'],request.form['titles1'],request.form['tags1'],request.form['summary1']))		
+			info = "new feed inserted"
+		else:
+			return render_template('db_table.html',info="feed link already existing",data=retrive(Data,'techfeeds'))
+	elif opt=='delete':
+		Data.execute('delete from feed where feed_no='+request.form['feedNo'])
+		info = "feed deleted"
+	elif opt=='update':
+		Data.execute("""update feed set feed_links = ? ,tags = ?,titles = ?,summary = ? where feed_no = ?""",(request.form['feedLinks'],request.form['tags'],request.form['titles'],request.form['summary'],request.form['feedNo']))
+		info = "feed updated"
+	Data.commit()
+	return render_template('db_table.html',data=retrive(Data,'techfeeds'),info=info)
+
+if __name__== "__main__":
+	app.run(port=5000)
